5193d5999dda14b4808d9820140caa218babb361
Vitor Oliveira <vipoliveira@gmail.com>
2018-08-27 23:42:17 -0300

    Creating project

---
+++ app/src/main/java/com/study/vipoliveira/githubinterface/GitListActivity.kt
@@ -0,0 +1,12 @@
+package com.study.vipoliveira.githubinterface
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GitListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_list)
+    }
+}
